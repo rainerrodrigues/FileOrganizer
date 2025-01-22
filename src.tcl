111bb9227f1cf90ef1364ebe6149f58272255e20
rainerrodrigues <rainerrodrigues16@gmail.com>
2025-01-22 00:33:34 +0000

    Added function for organizing files by date and for compressing files into tar.gz format

--- src.tcl
+++ src.tcl
@@ -30,4 +30,39 @@ proc organize_by_type {directory target_dir} {
 	}
 }
 
+# Organize files by size
+proc organize_by_size {directory target_dir} {
+	set files [list_files $directory]
+	foreach file $files {
+		set size [file size "$directory/$file"]
+		if {$size < 1024} {
+			set size_category "small"
+		} elseif {$size < 1048576} {
+			set size_category "medium"
+		} else {
+			set size_category "large"
+		}
+		set size_dir "$target_dir/$size_category"
+		ensure_directory $size_dir
+		file rename -force "$directory/$file" "$size_dir/"
+	}
+}
+
+# Organize files by creation date
+proc organize_by_date {directory target_dir} {
+	set files [list_files $directory]
+	foreach file $files {
+		set ctime [file attributes "$directory/$file" -creationtime]
+		set date_dir "$target_dir/[clock format $ctime -format %Y-%m-%d]"
+		ensure_directory $date_dir
+		file rename -force "$directory/$file" "$date_dir/"
+	}
+}
+
+# Backup files by compressing them into a .tar.gz archive
+proc backup_files {directory backup_file} {
+	set files [list_files $directory]
+	set tar_cmd "tar -czf $backup_file -C $directory ."
+	exec $tar_cmd
+}
 
